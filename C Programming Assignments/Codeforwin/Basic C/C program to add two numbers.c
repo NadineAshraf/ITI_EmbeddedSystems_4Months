d64e186f1b617d802043532838cd02d3e12328b5
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-10-28 16:42:17 +0200

    Create C program to add two numbers.c

---
+++ C Programming Assignments/Codeforwin/Basic C/C program to add two numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  int num1,num2;
+  printf("Input first number: ");
+  scanf("%d",&num1);
+  printf("Input second number: ");
+  scanf("%d",&num2);
+  printf("Sum = %d",num1+num2);
+   
+   
+    return 0;
+}
