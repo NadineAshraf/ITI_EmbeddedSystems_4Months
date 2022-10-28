810c78a9fb10499c34569be3b06a488b0542d269
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-10-28 16:48:33 +0200

    Create C program to perform all arithmetic operations.c

---
+++ C Programming Assignments/Codeforwin/Basic C/C program to perform all arithmetic operations.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+  int num1,num2;
+  printf("First number: ");
+  scanf("%d",&num1);
+  printf("Second number: ");
+  scanf("%d",&num2);
+  printf("Sum = %d\n",num1+num2);
+  printf("Difference = %d\n",num1-num2);
+  printf("Product = %d\n",num1*num2);
+  printf("Quotient = %d\n",num1/num2);
+  printf("Modulus = %d\n",num1%num2);
+   
+    return 0;
+}
