32779d6b10e16999d2776068c39847e9cd630bed
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-10-28 17:04:41 +0200

    Create 5. C program to find area of a rectangle.c

---
+++ C Programming Assignments/Codeforwin/Basic C/5. C program to find area of a rectangle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+   float length,width;
+   printf("Enter length: ");
+   scanf("%f",&length);
+   printf("Enter width: ");
+   scanf("%f",&width);
+   printf("Area of rectangle = %f sq. units", length*width);
+   
+    return 0;
+}
