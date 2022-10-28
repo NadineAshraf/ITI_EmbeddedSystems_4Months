ead2c0f46fb9219f1dafc340f608f9b247e42201
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-10-28 19:55:35 +0200

    Create 14. C program to find area of a triangle.c

---
+++ C Programming Assignments/Codeforwin/Basic C/14. C program to find area of a triangle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  float base,height;
+  printf("Enter base of the triangle: ");
+  scanf("%f",&base);
+  printf("Enter height of the triangle: ");
+  scanf("%f",&height);
+  printf("Area of the triangle = %.2f sq. units",(base*height)/2);
+    
+    
+    
+   
+    return 0;
+}
