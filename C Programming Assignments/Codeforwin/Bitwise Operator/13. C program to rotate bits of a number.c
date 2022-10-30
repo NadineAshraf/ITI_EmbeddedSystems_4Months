69467f5fc2890fab63a3b7925d68167c536ae3df
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-10-30 14:13:18 +0200

    Update 13. C program to rotate bits of a number.c

--- C Programming Assignments/Codeforwin/Bitwise Operator/13. C program to rotate bits of a number.c
+++ C Programming Assignments/Codeforwin/Bitwise Operator/13. C program to rotate bits of a number.c
@@ -1,1 +1,22 @@
-
+#include <stdio.h>
+int rotateRight(int num, int rotations)
+{
+    int Dropped_LSB;
+    num>>=1;
+    num = num & (~)
+}
+int main()
+{
+   int num,i,rotations;
+   printf("Input number = ");
+   scanf("%d",&num);
+   printf("Number of rotations = ");
+   scanf("%d",&rotations);
+ 
+  
+   printf("%d left rotated %d times = %d\n",num,rotations,);
+   printf("%d right rotated %d times = %d",num,rotations,);
+   
+   
+    return 0;
+}
