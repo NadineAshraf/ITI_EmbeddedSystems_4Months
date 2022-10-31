dc4f8e91f670bcde60034bfcfdf05ae4ef6acbf5
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-10-31 09:20:52 +0200

    Create 6. C program to check positive negative or zero using switch case.c

---
+++ C Programming Assignments/Codeforwin/Switch Case/6. C program to check positive negative or zero using switch case.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("Input number: ");
+    scanf("%d",&num);
+    switch(num>0)
+    {
+        
+        case 1:
+               printf("%d is positive",num);
+               break;
+        case 0:
+               switch(num<0)
+               {
+                case 1:
+                    printf("%d is negative",num);
+                    break;
+                case 0:
+                    printf("%d is zero",num);
+                    break;
+               }
+      
+    }
+
+    return 0;
+}
