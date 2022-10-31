38b6db3706ca5a7a6c5a240ca82bb5cc8a299627
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-10-31 11:02:58 +0200

    Create 7. C program to find sum of even numbers between 1 to n.c

---
+++ C Programming Assignments/Codeforwin/Loop and iteration/7. C program to find sum of even numbers between 1 to n.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main ()
+{
+  int num,i,sum=0;
+  printf("Input upper limit of even number: ");
+  scanf("%d",&num);
+  printf("Sum of even numbers between 1 to 10: ");
+  for(i=0; i<=num; i+=2)
+  {
+    sum+=i;
+  }
+  printf("%d",sum);
+
+  return 0;
+}
