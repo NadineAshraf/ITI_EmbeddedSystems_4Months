a149334f59503ced7b42bd4136b0d2c69631d17a
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-11-03 06:54:15 +0200

    Create 22. C program to find all factors of a number.c

---
+++ C Programming Assignments/Codeforwin/Loop and iteration/22. C program to find all factors of a number.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+   int num,i,flag=0;
+   printf("Input number: ");
+   scanf("%d",&num);
+   printf("Factors of %d: ",num);
+   for(i=1; i<=num; i++)
+   {
+       if(num == i)
+       {
+           printf("%d", i);
+       }
+       else if((num%i) == 0)
+       {
+           printf("%d, ", i);
+       }
+     
+   }
+    return 0;
+}
