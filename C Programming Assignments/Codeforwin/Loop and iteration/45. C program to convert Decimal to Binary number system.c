4847448efdb1e85e5d0d7177a14e3cffb2ccf81e
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-11-03 19:37:58 +0200

    Update 45. C program to convert Decimal to Binary number system.c

--- C Programming Assignments/Codeforwin/Loop and iteration/45. C program to convert Decimal to Binary number system.c
+++ C Programming Assignments/Codeforwin/Loop and iteration/45. C program to convert Decimal to Binary number system.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int temp,digit,num,i,j=0,length,index=0;
+   char outbin[65],binary[65];
+    printf("Input deciaml number: ");
+    scanf("%d",&num);
+    temp=num;
+   
+    while(temp != 0)
+    {
+        binary[index]= (temp%2)+'0';
+        temp/=2;
+        index++;
+    }
+  
+    
+length =strlen(binary);
+i = length -1;
+while(i>=0)
+{
+    outbin[j] = binary[i];
+    i--;
+    j++;
+}
+printf("Binary number: %s",outbin);
 
+
+    return 0;
+}
