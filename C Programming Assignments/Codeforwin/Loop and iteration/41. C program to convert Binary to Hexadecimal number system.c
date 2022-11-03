ba1c85731a6eb9e9a3cffb414a5074a00407378e
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-11-03 16:40:33 +0200

    Update 41. C program to convert Binary to Hexadecimal number system.c

--- C Programming Assignments/Codeforwin/Loop and iteration/41. C program to convert Binary to Hexadecimal number system.c
+++ C Programming Assignments/Codeforwin/Loop and iteration/41. C program to convert Binary to Hexadecimal number system.c
@@ -1,1 +1,42 @@
-
+#include <stdio.h>
+#include <string.h>
+#define SIZE 16
+int main()
+{
+    int hexConstant [] = {0000,0001,0010,0011,0100,0101,0110,0111,1000,1001,1010,1011,1100,1101,1110,1111};
+    int temp,digit,num,i,index=0,flag=0;
+    char hex[20];
+    printf("Input binary number: ");
+    scanf("%d",&num);
+    temp=num;
+    while(temp != 0)
+    {
+        digit= temp%10000;
+       for(i=0; i<SIZE;i++)
+       {
+           if(hexConstant[i]==digit)
+           {
+              if(i<10)
+              {
+                  hex[index] = (char)(i+48);
+              }
+              else
+              {
+                  hex[index] = (char)((i-10)+65);
+              }
+              index++;
+              flag=1;
+              break;
+           }
+       }
+        
+        temp/=10000;
+       
+    }
+     
+    if(flag == 1)
+    {
+    printf("%s",hex);
+    }
+    return 0;
+}
