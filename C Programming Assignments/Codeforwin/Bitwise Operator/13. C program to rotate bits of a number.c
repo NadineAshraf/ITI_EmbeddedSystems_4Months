9f6e02515966c38ead2dcd0790cfee9986a9d608
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-11-03 05:52:45 +0200

    Update 13. C program to rotate bits of a number.c

--- C Programming Assignments/Codeforwin/Bitwise Operator/13. C program to rotate bits of a number.c
+++ C Programming Assignments/Codeforwin/Bitwise Operator/13. C program to rotate bits of a number.c
@@ -1,9 +1,40 @@
 #include <stdio.h>
+#define INT_BITS sizeof(int)*8-1
 int rotateRight(int num, int rotations)
 {
     int Dropped_LSB;
-    num>>=1;
-    num = num & (~)
+    /* The effective rotation */
+    rotations %= INT_BITS;
+    /* Loop till rotation becomes 0 */
+    while(rotations--)
+    {
+        /*Get LSB of num before it gets dropped */
+        Dropped_LSB = num & 1;  
+        /* Right shift by 1 and
+           clear its MSB */
+        num = (num >> 1) & (~(1 << INT_BITS));
+        /* Set its dropped LSB as new MSB */
+        num = num | (Dropped_LSB << INT_BITS);
+        
+    }
+    return num;
+    
+}
+int rotateLeft(int num,int rotations)
+{
+    int Dropped_MSB;
+    /* The effective rotation*/
+    rotations %= INT_BITS;
+    /* loop till rotation becomes 0*/
+    while(rotations--)
+    {
+        /* Get MSB of num before it gets dropped */
+        Dropped_MSB = (num >> INT_BITS) & 1;
+        /* Left rotate num by 1 and
+           set its dropped MSB as new LSB */
+        num = (num << 1) | Dropped_MSB;
+    }
+    return num;
 }
 int main()
 {
@@ -14,8 +45,8 @@ int main()
    scanf("%d",&rotations);
  
   
-   printf("%d left rotated %d times = %d\n",num,rotations,);
-   printf("%d right rotated %d times = %d",num,rotations,);
+   printf("%d left rotated %d times = %d\n",num,rotations, rotateLeft(num,rotations));
+   printf("%d right rotated %d times = %d",num,rotations, rotateRight(num, rotations));
    
    
     return 0;
