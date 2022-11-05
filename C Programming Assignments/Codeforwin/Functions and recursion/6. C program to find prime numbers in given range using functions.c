3e2c604238f06442c6707c7bf14ef14ef3f01afa
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-11-05 06:35:12 +0200

    Update 6. C program to find prime numbers in given range using functions.c

--- C Programming Assignments/Codeforwin/Functions and recursion/6. C program to find prime numbers in given range using functions.c
+++ C Programming Assignments/Codeforwin/Functions and recursion/6. C program to find prime numbers in given range using functions.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
 
+int prime(int num)
+{
+    int i,flag=0;
+    for(i=2;i<num;i++)
+    {
+        if(num%i == 0)
+        {
+           return 0;
+        }
+    
+     }     
+    if (flag == 0)
+    {
+        return 1;
+    }
+}
+void LimitPrime(int lower, int upper)
+{
+    printf("Prime numbers between %d-%d are: ",lower,upper);
+    while(lower<=upper)
+    {
+        if(prime(lower))
+        {
+            printf("%d, ",lower);
+        }
+        lower++;
+    }
+}
+
+int main()
+{
+     int lower,upper;
+    printf("Input lower limit: ");
+    scanf("%d",&lower);
+    printf("Input upper limit: ");
+    scanf("%d",&upper);
+    LimitPrime(lower,upper);
+    
+    return 0;
+}
