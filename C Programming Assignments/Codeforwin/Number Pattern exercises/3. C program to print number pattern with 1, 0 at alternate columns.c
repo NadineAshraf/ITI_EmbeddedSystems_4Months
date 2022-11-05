36932f8256909b07982f530d6c5051c68941ae33
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-11-05 18:54:01 +0200

    Create 3. C program to print number pattern with 1, 0 at alternate columns.c

---
+++ C Programming Assignments/Codeforwin/Number Pattern exercises/3. C program to print number pattern with 1, 0 at alternate columns.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+int main()
+{
+   int rows,columns,i,j;
+   printf("Input rows: ");
+   scanf("%d",&rows);
+   printf("Input columns: ");
+   scanf("%d",&columns);
+   for(i=0; i<rows; i++)
+   {
+       for(j=0; j<columns; j++)
+       {
+           if(j%2 !=0)
+           {
+            printf("%d",1);
+           }
+           else
+           {
+               printf("%d",0);
+           }
+       }
+       printf("\n");
+   }
+
+    return 0;
+}
