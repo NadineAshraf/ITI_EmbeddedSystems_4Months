40576d8acd597658ecbc1bce8b23f46ba8ca2a76
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-11-05 08:12:57 +0200

    Update 7. C program to print all strong numbers between given interval using function.c

--- C Programming Assignments/Codeforwin/Functions and recursion/7. C program to print all strong numbers between given interval using function.c
+++ C Programming Assignments/Codeforwin/Functions and recursion/7. C program to print all strong numbers between given interval using function.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
+long long factorial(int num)
+{
+    if(num == 0)
+        return 1;
+    else
+        return (num * factorial(num-1));
+}
+void LimitStrong(int lower, int upper)
+{
+    int last,OriginalNum;
+    long long sum;
+    
+    while(lower!=upper)
+    {
+        sum=0;
+        OriginalNum=lower;
+        while(OriginalNum !=0)
+        {
+          last =OriginalNum%10;
+          sum+=factorial(last);
+          OriginalNum/=10;
+        }
+        if(lower == sum )
+       {
+           printf("%d, ",lower);
+       }
+       lower++;
+    }
+}
 
+int main()
+{
+     int lower,upper;
+    printf("Input lower limit: ");
+    scanf("%d",&lower);
+    printf("Input upper limit: ");
+    scanf("%d",&upper);
+    printf("Strong numbers between %d-%d are: ",lower,upper);
+    LimitStrong(lower,upper);
+  
+    return 0;
+}
