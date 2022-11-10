7abf76a159efb22dc6100a41b95a9ed5599b677f
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-11-10 08:40:35 +0200

    Update 1. RobotSimulator.cpp

--- C++ Assignments/Exercism/1. RobotSimulator.cpp
+++ C++ Assignments/Exercism/1. RobotSimulator.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+void SetDirection(char *Copy_ptrToCharDirection, char Copy_CharRequiredDirection);
+void RobotMove(int *Copy_IntXlocation, int *Copy_intYlocation, char *Copy_ptrToCharDirection);
+void SetDirection(char *Copy_ptrToCharDirection, char Copy_CharRequiredDirection)
+{
+    
+}
+void RobotMove(int *Copy_IntXlocation, int *Copy_intYlocation, char *Copy_ptrToCharDirection)
+{
+    int i;
+    for()
+}
+int main() {
+    int local_intXlocation = 7, local_intYlocation = 3;
 
+    return 0;
+}
