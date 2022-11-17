d74847cf1a68c1135996d37d43327c4cabbada27
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-11-17 20:08:09 +0200

    Create 38. C program to check Identity matrix.c

---
+++ C Programming Assignments/Codeforwin/Array & Matrix/38. C program to check Identity matrix.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#define ROW  3
+#define COL  3
+#define SIZE 3
+  
+int main()
+{
+    int arr[SIZE][SIZE],i,j,flag=0;
+    printf("Input array elements: ");
+    for(i=0; i<ROW; i++)
+    {
+        for(j=0; j<COL; j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+     for(i=0; i<ROW; i++)
+    {
+        for(j=0; j<COL; j++)
+        {
+            if( i == j && arr[i][j] == 1)
+            {
+                flag=1;
+            }
+        }
+    }
+if(flag == 1)
+{
+    printf("It is an Identity matrix");
+}
+else
+{
+    printf("It is not an Identity matrix");
+}
+ 
+    
+    return 0;
+}
