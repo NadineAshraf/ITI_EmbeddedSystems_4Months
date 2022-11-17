cb6e13a8f1eea928b0d352439492aa6e1bee86dc
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-11-17 17:58:54 +0200

    Create 30. C program to find sum of each row and columns of a matrix.c

---
+++ C Programming Assignments/Codeforwin/Array & Matrix/30. C program to find sum of each row and columns of a matrix.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#define ROW  3
+#define COL  3
+
+  
+int main()
+{
+    int arr[SIZE][SIZE],i,j,sum;
+    printf("Input array elements: ");
+    for(i=0; i<ROW; i++)
+    {
+        for(j=0; j<COL; j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    
+     for(i=0; i<ROW; i++)
+    {
+        sum=0;
+        for(j=0; j<COL; j++)
+        {
+           sum += arr[i][j];
+           
+        }
+        printf("Sum of row %d = %d\n",i+1,sum);
+    }
+     for(i=0; i<ROW; i++)
+    {
+        sum=0;
+        for(j=0; j<COL; j++)
+        {
+           sum += arr[j][i];
+           
+        }
+        printf("Sum of column %d = %d\n",i+1,sum);
+    }
+    return 0;
+}
