a78a1da615e2f781338e86a96b36c116b5a25b7a
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-11-21 13:01:10 +0200

    Create 1. C++ Constructor and Destructor.cpp

---
+++ C++ Assignments/CodezClub/C++ Constructor and Destructor/1. C++ Constructor and Destructor.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+class Cube
+{
+    private:
+         double side;
+    public:
+    void Input();
+    double volume();
+     Cube();
+    ~Cube();
+};
+
+void Cube::Input()
+{
+    cout<<"Please enter side length: ";
+    cin>>side;
+}
+double Cube::volume()
+{
+    return side*side*side;
+}
+Cube:: Cube()
+{
+    cout<<"A default constructor is called "<<endl;
+  
+}
+Cube::~Cube()
+{
+    cout<<"\nDestructing: "<<side<<endl;
+}
+int main()
+{
+   Cube c1;
+   double vol;
+   cout<<"Enter the side length:\n";
+   c1.Input();
+   cout<<"Enter volume of cube: ";
+   cout<<c1.volume();
+  
+
+    return 0;
+}
