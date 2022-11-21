da8221b5909d494e052b274bd6910b124fc87097
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-11-21 06:35:20 +0200

    Create Input and Output.cpp

---
+++ C++ Assignments/HackerRank/Input and Output.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+     int a,b,c;
+     cin>>a>>b>>c;
+     int stdin[3]={a,b,c};
+     int stdout=a+b+c;
+     cout<<stdout;
+    return 0;
+}
