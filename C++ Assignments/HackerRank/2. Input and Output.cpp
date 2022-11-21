448087acdcb56cf63b3e7f963f9522fc12f0b7ba
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-11-21 06:36:23 +0200

    Update and rename Input and Output.cpp to 2. Input and Output.cpp

--- C++ Assignments/HackerRank/2. Input and Output.cpp
+++ C++ Assignments/HackerRank/2. Input and Output.cpp
@@ -11,8 +11,8 @@ int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
      int a,b,c;
      cin>>a>>b>>c;
-     int stdin[3]={a,b,c};
      int stdout=a+b+c;
      cout<<stdout;
     return 0;
 }
+
