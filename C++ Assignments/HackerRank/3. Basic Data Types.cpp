1a567606f73724fcf2b1bb48c336eceee38bff84
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-11-21 06:39:59 +0200

    Create 3. Basic Data Types.cpp

---
+++ C++ Assignments/HackerRank/3. Basic Data Types.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int x;
+    long y;
+    char z;
+    float f;
+    double I;
+    cin>>x>>y>>z>>f>>I;
+    cout<<x<<endl<<y<<endl<<z<<endl;
+    cout<<setprecision(7)<<f<<endl;
+    cout<<setprecision(17)<<I<<endl;
+    return 0;
+}
