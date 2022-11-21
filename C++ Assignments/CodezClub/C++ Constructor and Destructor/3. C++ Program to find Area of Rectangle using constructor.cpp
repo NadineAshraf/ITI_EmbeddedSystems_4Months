533ecb200708077e0d73cfd2bf286fb58e5d2be4
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-11-21 14:01:37 +0200

    Update 3. C++ Program to find Area of Rectangle using constructor.cpp

--- C++ Assignments/CodezClub/C++ Constructor and Destructor/3. C++ Program to find Area of Rectangle using constructor.cpp
+++ C++ Assignments/CodezClub/C++ Constructor and Destructor/3. C++ Program to find Area of Rectangle using constructor.cpp
@@ -1,1 +1,47 @@
+#include <iostream>
 
+using namespace std;
+class RectArea
+{
+    private:
+         float length;
+         float width;
+    public:
+    void Input();
+    double Area();
+     RectArea();
+    ~RectArea();
+};
+
+void RectArea::Input()
+{
+    cout<<"Please enter length of rectangle: ";
+    cin>>length;
+    cout<<"Please enter width of rectangle: ";
+    cin>>width;
+}
+double RectArea::Area()
+{
+    return length*width;
+}
+RectArea::RectArea()
+{
+    
+}
+
+RectArea::~RectArea()
+{
+    
+}
+
+int main()
+{
+   RectArea c1;
+   cout<<"Enter the Dimensions:\n";
+   c1.Input();
+   cout<<"Area of rectangle: ";
+   cout<<c1.Area();
+  
+
+    return 0;
+}
