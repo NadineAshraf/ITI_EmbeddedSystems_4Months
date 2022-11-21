61273eb3eecbbd6305d11ea509741a40721480e2
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-11-21 10:40:07 +0200

    Create 1. C++ Program to find Factorial of a number using class.cpp

---
+++ C++ Assignments/CodezClub/C++ Classes and Objects/1. C++ Program to find Factorial of a number using class.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+class Factorial
+{
+    private:
+         int n,factorial=1;
+    public:
+    void input();
+    void calc();
+    void Print();
+};
+void Factorial::input()
+{
+    cout<<"Please enter the number: ";
+    cin>>n;
+}
+void Factorial::calc()
+{
+    while(n!=0)
+    {
+    factorial*=n;
+    n--;
+    }
+    
+}
+void Factorial::Print()
+{
+    cout<<"\nFactorial = "<<factorial;
+}
+int main()
+{
+   Factorial factorial;
+   factorial.input();
+   factorial.calc();
+   factorial.Print();
+   
+
+    return 0;
+}
