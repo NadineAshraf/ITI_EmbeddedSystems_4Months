ccae356b46f7e958375a55ccea4ff5456a244a6a
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-12-12 15:16:43 +0200

    Create H. Two numbers.cpp

---
+++ C++ Assignments/Codeforces/Sheet1/H. Two numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+   float A,B;
+   cin>>A>>B;
+   cout<<"floor "<<A<<" / "<<B<<" = "<<floor(A/B)<<"\n";
+  cout<<"ceil "<<A<<" / "<<B<<" = "<<ceil(A/B)<<"\n";
+  cout<<"round "<<A<<" / "<<B<<" = "<<round(A/B)<<"\n";
+    return 0;
+}
