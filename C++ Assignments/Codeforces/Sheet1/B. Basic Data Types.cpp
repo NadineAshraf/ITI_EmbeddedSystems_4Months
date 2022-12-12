8514d431ca0a2ae96313d932f76682836bcce395
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-12-12 14:47:50 +0200

    Create B. Basic Data Types.cpp

---
+++ C++ Assignments/Codeforces/Sheet1/B. Basic Data Types.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int x;
+   long y;
+   char ch;
+   float f;
+   double d;
+   cin>>x>>y>>ch>>f>>d;
+   cout<<x<<"\n"<<y<<"\n"<<ch<<"\n"<<f<<"\n"<<d<<endl;
+
+    return 0;
+}
