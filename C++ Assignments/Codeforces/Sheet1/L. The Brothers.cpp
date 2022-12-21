9ae7fa789521b0afde21764db8d86b07bccc3c06
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-12-21 06:30:28 +0200

    Create L. The Brothers.cpp

---
+++ C++ Assignments/Codeforces/Sheet1/L. The Brothers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s1,s2,s3,s4;
+    cin>>s1>>s2>>s3>>s4;
+    if(s2 ==s4)
+    {
+        cout<<"ARE Brothers";
+    }
+    else
+    {
+        cout<<"NOT";
+    }
+    
+   
+    return 0;
+}
