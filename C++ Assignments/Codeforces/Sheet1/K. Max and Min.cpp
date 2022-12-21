b9d7ab005785d05f681780cb22b0f75a9ba4287a
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-12-21 06:24:20 +0200

    Create K. Max and Min.cpp

---
+++ C++ Assignments/Codeforces/Sheet1/K. Max and Min.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    signed long long int A,B,C;
+    cin>>A>>B>>C;
+    signed long long int max,min;
+    if(A>B && A>C)
+    {
+        max = A;
+    }
+    else if(B>A && B>C)
+    {
+        max = B;
+    }
+    else if(C>A && C>B)
+    {
+        max = C;
+    }
+    if(A<B && A<C)
+    {
+        min = A;
+    }
+    else if(B<A && B<C)
+    {
+        min = B;
+    }
+    else if(C<A && C<B)
+    {
+        min = C;
+    }
+    cout<<min<<" "<<max;
+    return 0;
+}
