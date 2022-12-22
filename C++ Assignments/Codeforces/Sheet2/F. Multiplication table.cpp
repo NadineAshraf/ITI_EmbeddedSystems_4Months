cf6cdc3d6b642884d224e5b4f0815a9eb2eb0dfd
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-12-22 11:38:52 +0200

    Create F. Multiplication table.cpp

---
+++ C++ Assignments/Codeforces/Sheet2/F. Multiplication table.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,i;
+    cin>>n;
+    for(i=1; i<=12; i++)
+    {
+        cout<<n<<" * "<<i<<" = "<<n*i<<"\n";
+    }
+
+    return 0;
+}
