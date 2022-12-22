133b6d00e35e7139afe0e53a1a46257c8eae140a
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2022-12-22 10:36:44 +0200

    Create C. Even, Odd, Positive and Negative.cpp

---
+++ C++ Assignments/Codeforces/Sheet2/C. Even, Odd, Positive and Negative.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int N,Even=0,odd=0,pos=0,neg=0;
+   int i;
+   cin>>N;
+  signed long long int arr[N];
+  for(i=0; i<N; i++)
+  {
+      cin>>arr[i];
+      if(arr[i] %2 == 0)
+      {
+          Even++;
+      }
+      else
+      {
+          odd++;
+      }
+      if(arr[i]>0)
+      {
+          pos++;
+      }
+      else if(arr[i]<0)
+      {
+          neg++;
+      }
+  }
+  cout<<"Even: "<<Even<<"\n";
+  cout<<"Odd: "<<odd<<"\n";
+  cout<<"Positive: "<<pos<<"\n";
+  cout<<"Negative: "<<neg<<"\n";
+   
+
+}
