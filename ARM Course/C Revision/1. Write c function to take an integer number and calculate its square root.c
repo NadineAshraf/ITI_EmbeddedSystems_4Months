3d337e116969aec7110445783a46fb1ab8f4e7fe
Nadine Ashraf Adeeb <91349300+NadineAshraf@users.noreply.github.com>
2023-01-27 08:32:03 +0200

    Update and rename 1. to 1. Write c function to take an integer number and calculate its square root.c

---
+++ ARM Course/C Revision/1. Write c function to take an integer number and calculate its square root.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+float SquareRoot(int num)
+{
+    int start = 0,end = num,mid;
+    float ans;
+    while(start<=end)
+    {
+        mid = (start+end)/2;
+    if(mid*mid == num)
+    {
+        ans = mid;
+        break;
+    }
+    if(mid*mid < num)
+    {
+        ans = start;
+        start = mid+1;
+    }
+    else
+    {
+        end = mid -1;
+    }
+}
+    float increment = 0.1;
+    for(int i=0; i<5; i++)
+    {
+        while(ans*ans <= num)
+        {
+            ans+= increment;
+        }
+        ans -= increment;
+        increment/=10;
+    }
+    return ans;
+}
+int main()
+{
+  printf("%f",SquareRoot(12));
+
+    return 0;
+}
+
